899b35806b8549b1dd1f7bf38421d07298a24eb3
Shourya Eaga <shourya.eaga.09@gmail.com>
2025-06-27 22:04:12 +0100

    change to madgwick

--- visualiser/main.py
+++ visualiser/main.py
@@ -1,6 +1,9 @@
 import argparse
 from pyray import *
 import serial
+from ahrs.filters import Madgwick
+import numpy as np
+from ahrs.common.orientation import q2euler
 
 ax = 0
 ay = 0
@@ -8,6 +11,12 @@ az = 0
 gx = 0
 gy = 0
 gz = 0
+
+
+dt = 0.05
+
+madgwick = Madgwick()
+
 
 def main():
     parser = argparse.ArgumentParser(description="Python script to visualise incoming serial data about acceleration and rotation")
@@ -20,20 +29,25 @@ def main():
 
     ser = serial.Serial(args.port, args.baudrate)
 
-    screen_width = 800
-    screen_height = 450
+    screen_width = 1280
+    screen_height = 720
     init_window(screen_width, screen_height, b"Serial Data Visualisation")
 
     camera = Camera3D()
-    camera.position = Vector3(10, 10, 0)
+    camera.position = Vector3(20, 20, 0)
     camera.target = Vector3(0, 0, 0)
     camera.up = Vector3(0, 1, 0)
     camera.fovy = 45.0
     camera.projection = CameraProjection.CAMERA_PERSPECTIVE
 
-    cubeVelocity = Vector3(0, 0, 0)
+    cubeMesh = gen_mesh_cube(1.0, 1.0, 1.0)
+    cubeModel = load_model_from_mesh(cubeMesh)
+    
+
 
     cubePosition = Vector3(0, 0, 0)
+
+    q = np.array([1.0, 0.0, 0.0, 0.0], dtype=np.float64)  # Initial quaternion
 
     set_target_fps(20)
 
@@ -44,47 +58,46 @@ def main():
         if ser.in_waiting > 0:
             try:
                 line = ser.readline().decode('utf-8').strip()
+                print(f"Received line: {line}")
                 data = line.split(',')
-                ax = round(float(data[0]), 2)
-                ay = round(float(data[1]), 2)
-                az = round(float(data[2]), 2)
-                gx = round(float(data[3]), 2)
-                gy = round(float(data[4]), 2)
-                gz = round(float(data[5]), 2)
+                print(f"Parsed data: {data}")
+                raw_ax = float(data[0])
+                raw_ay = float(data[1])
+                raw_az = float(data[2])
+                print(f"Raw Data: ax={raw_ax}, ay={raw_ay}, az={raw_az}")
+                gx = float(data[3])
+                gy = float(data[4])
+                gz = float(data[5])
                 # print(f"Acceleration: ax={ax}, ay={ay}, az={az} | Gyroscope: gx={gx}, gy={gy}, gz={gz}")
                 # Swap axes y and z due to the coordinate system differences
-                temp = ay
-                ay = az
-                az = temp
-            except:
+
+                acc = np.array([raw_ax, raw_ay, raw_az])
+                gyro = np.array([gx, gy, gz])
+
+                q = madgwick.updateIMU(q, gyro, acc)
+
+                roll, pitch, yaw = q2euler(q)
+                print(f"Orientation: Roll={roll:.2f}, Pitch={pitch:.2f}, Yaw={yaw:.2f}")
+            except IndexError or ValueError:
                 print(f"Error parsing line: {line}")
                 continue
         else:
             ax = ay = az = 0
         
+        # Update cube position based on orientation
 
-        # Make sure y is negative
-        ay = -ay
-        ay += 9.79  # Adjust for gravity
-        print(f"Adjusted Acceleration: ax={ax}, ay={ay}, az={az}")
 
-        cubeVelocity.x += ax * 0.05
-        cubeVelocity.y += ay * 0.05
-        cubeVelocity.z += az * 0.05
+
+
         
-        # print(f"Cube Velocity: {cubeVelocity.x}, {cubeVelocity.y}, {cubeVelocity.z}")
-
-        cubePosition.x += cubeVelocity.x * 0.05
-        cubePosition.y += cubeVelocity.y * 0.05
-        cubePosition.z += cubeVelocity.z * 0.05
+        cubeModel.transform = matrix_rotate_xyz(Vector3(-pitch, -yaw, -roll))
 
         # Draw
         begin_drawing()
         clear_background(RAYWHITE)
         
         begin_mode_3d(camera)
-        draw_cube(cubePosition, 1.0, 1.0, 1.0, RED)
-        draw_cube_wires(cubePosition, 1.0, 1.0, 1.0, MAROON)
+        draw_model_ex(cubeModel, cubePosition, Vector3(0, 0, 1), 1.0, Vector3(1.0, 1.0, 1.0), RED)
         draw_grid(10, 1.0)
         end_mode_3d()
 
